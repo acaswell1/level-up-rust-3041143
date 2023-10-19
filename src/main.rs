e18e4c218cd5a28f26e727e709bfdfe4db9856fe
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-10-19 19:00:46 +0000

    Challenge 3 complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-fn info(a: &T) {
-    todo!();
+fn info<T : std::fmt::Debug>(a: &T) {
+    println!("{:?}", a);
 }
 
 fn main() {
@@ -8,16 +8,15 @@ fn main() {
     info(&a);
     info(&b);
 
-    // Advanced 1
-    // use std::ffi::CString;
-    
-    // let c = CString::new("?").unwrap();
-    // info(&input);
+    //Advanced 1
+    use std::ffi::CString;
+    let c = CString::new("?").unwrap();
+    info(&c);
 
     // Advanced 2
-    // use std::path::Path;
-    // let d = Path::new("/tmp/linkedin-learning");
-    // info(d);
+    use std::path::Path;
+    let d = Path::new("/tmp/linkedin-learning");
+    info(&d);
 }
 
 
@@ -33,22 +32,22 @@ fn string() {
     info(&input);
 }
 
-// #[test]
-// fn chars() {
-//     let input = 'r';
-//     info(&input);
-// }
+#[test]
+fn chars() {
+    let input = 'r';
+    info(&input);
+}
 
-// #[test]
-// fn cstring() {
-//     use std::ffi::{CString};
-//     let input = CString::new("Rust").unwrap();
-//     info(&input);
-// }
+#[test]
+fn cstring() {
+    use std::ffi::{CString};
+    let input = CString::new("Rust").unwrap();
+    info(&input);
+}
 
-// #[test]
-// fn path() {
-//     use std::path::Path;
-//     let input = Path::new("/tmp/rust");
-//     info(input);
-// }
+#[test]
+fn path() {
+    use std::path::Path;
+    let input = Path::new("/tmp/rust");
+    info(&input);
+}
