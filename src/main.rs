7c5adbd6c2c76f6845470b5e708050d823300e4b
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-10-30 20:17:50 +0000

    superior solution to the video

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use chrono::prelude::*;
+
 fn weeks_between(a: &str, b: &str) -> i32 {
-    todo!()
+    match NaiveDate::parse_from_str(a, "%Y-%m-%d") {
+        Ok(birth_a) => match NaiveDate::parse_from_str(b, "%Y-%m-%d") {
+            Ok(birth_b) => (birth_b - birth_a).num_weeks().try_into().unwrap_or_default(),
+            Err(_) => { println!("Unpredicted: 1 Error"); -1 },
+        },
+        Err(_) => { println!("Unpredicted: 2 Error"); -1 },
+    }
 }
 
 fn main() {
@@ -17,6 +25,7 @@ fn same_day() {
 #[test]
 fn one_week() {
     let n_weeks = weeks_between("1010-10-10", "1010-10-18");
+    println!("{}", n_weeks);
     assert_eq!(n_weeks, 1);
 }
 
