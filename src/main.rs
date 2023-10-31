9fd60182587647b5f138585a8afc0bd35f9ccf6b
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-10-31 15:43:29 +0000

    rgb solution

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,36 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+struct Rgb {
+    red : u8,
+    green : u8,
+    blue : u8,
+} // TODO: design data structure
 
 trait RgbChannels {
     fn r(&self) -> u8;
-
     fn g(&self) -> u8;
-
     fn b(&self) -> u8;
 }
 
 impl RgbChannels for Rgb {
-    // TODO: implement trait
+    fn r(&self) -> u8 { self.red }
+    fn g(&self) -> u8 { self.green }
+    fn b(&self) -> u8 { self.blue } 
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 7 {
+            Err("Error: Invalid Hex Color".to_string())
+        } else {
+            match hex::decode(s.replace('#', "")) {
+                Ok(rgb) => Ok(Rgb { red: rgb[0], green: rgb[1], blue: rgb[2] }),
+                Err(_) => Err("Error: Error Decoding Hex Values".to_string()),
+            }
+        }
+    }
 }
 
 impl Display for Rgb {
@@ -27,7 +41,7 @@ impl Display for Rgb {
 }
 
 fn main() {
-    // 
+    println!("{:?}", hex::decode("aabbcc"));
 }
 
 #[test]
@@ -36,6 +50,7 @@ fn every_color() {
 
     for ((r, g), b) in colors {
         let hex = format!("#{:02x}{:02x}{:02x}", r, g, b);
+        println!("{}", hex);
         let color: Rgb = hex.parse().unwrap();
         assert_eq!(hex, format!("{}", color));
     }
