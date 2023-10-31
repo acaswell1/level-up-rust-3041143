01e4672bb0b0d3c8251a708d8df5fb79fcb3b091
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-10-31 17:19:23 +0000

    runtime encoding

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,63 @@
 mod run_length_encoding {
     pub fn encode(text: &str) -> String {
-        todo!()
+        let mut res: String = "".to_string();
+        let mut num_occs: u8 = 0;
+        
+        let mut text_iter = text.chars();
+        let mut next_opt = text_iter.next();
+        let mut prev_char: char = next_opt.unwrap_or_default();
+
+        while  next_opt.is_some() {
+            let curr_char = next_opt.unwrap();    
+            next_opt = text_iter.next();
+
+            if  curr_char == prev_char && num_occs != 9 {
+                num_occs += 1;
+                prev_char =  curr_char;
+            } else {
+                res = res + &num_occs.to_string() + &prev_char.to_string();
+                num_occs = 1;
+                prev_char = curr_char; 
+            }
+
+            if next_opt.is_none() {
+                res = res + &num_occs.to_string() + &prev_char.to_string();
+            }
+        }
+        res
     }
-    
+
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut res: String = "".to_string();
+        let mut text_iter = text.chars();
+        let mut num_occs_iter = text_iter.next();
+        let mut char_iter = text_iter.next();
+
+        while  char_iter.is_some() {
+            let num_occs = num_occs_iter.unwrap();
+            let char = char_iter.unwrap();
+            let size = num_occs.to_digit(10).unwrap() as usize;
+
+            res += &char.to_string().repeat(size);
+
+            num_occs_iter = text_iter.next();
+            char_iter = text_iter.next();
+        }
+        res
     }
 }
 
 fn main() {
-    // 
+    use run_length_encoding::*;
+    //println!("{}", encode("abc"));
+
+    let input = encode("AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA");
+    println!("{}", decode(&input));
+
+    let input = encode("LinkedIn");
+    println!("{}", decode(&input));
+
+
 }
 
 #[test]
